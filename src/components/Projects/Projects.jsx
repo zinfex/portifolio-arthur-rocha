dcde7f2195f43336170ddf829c807bcf0de4e629
Arthur Rocha <zinfegr@gmail.com>
2025-03-20 12:06:14 -0300

    ajuste layout

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import { SiTypescript } from "react-icons/si";
 import { IoLogoJavascript } from "react-icons/io5";
 import { FaReact } from "react-icons/fa";
-import { RiNextjsFill } from "react-icons/ri";
+import { RiNextjsFill, RiNodejsFill, RiTailwindCssFill } from "react-icons/ri";
 import { FaNodeJs } from "react-icons/fa";
 
 
@@ -17,18 +17,24 @@ export default function Projects() {
     {
       title: 'Simple Forms',
       description: 'Criador de formulários inovador, simples e prático',
-      stacks: '',
+      stacks: [
+        <FaReact size={20} />, <IoLogoJavascript size={20} />
+      ],
       link: 'https://github.com/zinfex/portifolio'
     },
     {
       title: 'TempSync',
-      stacks: '',
+      stacks: [
+        <FaReact size={20} />, <IoLogoJavascript size={20} />, <RiNodejsFill size={20} />
+      ],
       description: 'Software de monitoramento de temperatura e umidade',
       link: 'https://github.com/zinfex/portifolio'
     },
     {
       title: 'BYD Site',
-      stacks: '',
+      stacks: [
+        <RiNextjsFill size={20} />, <SiTypescript size={20} />, <RiTailwindCssFill size={20} />
+      ],
       description: 'Criei uma nova versão do site da marca de carros BYD',
       link: 'https://github.com/zinfex/portifolio'
     },
@@ -40,12 +46,19 @@ export default function Projects() {
             {projects.map((project, index) => {
             return (
                 <div key={index} className="project-container">
-                    <h2>{project.title}</h2>
-                    <p>{project.description}</p>
-                    <div className="project-stacks">{project.stacks}</div>
-                    <a href={project.link} target="_blank" rel="noopener noreferrer" style={{display: 'flex', alignItems: 'center', gap: 5}}>
-                    <IoIosLink /> Ver Projeto
-                    </a>
+                    <h2 className="project-title">{project.title}</h2>
+                    <p className="project-description">{project.description}</p>
+                    
+                    <div style={{display: 'flex', alignItems: 'end', justifyContent: 'space-between'}}>
+                      <a className="project-link" href={project.link} target="_blank" rel="noopener noreferrer" style={{display: 'flex', alignItems: 'center', gap: 5}}>
+                        <IoIosLink /> Ver Projeto
+                      </a>
+                      <div className="project-stacks">
+                        {project.stacks.map((stackIcon, i) => (
+                          <span key={i} style={{ marginRight: 8 }}>{stackIcon}</span>
+                        ))}
+                      </div>
+                    </div>
                 </div>
             )
             })}
